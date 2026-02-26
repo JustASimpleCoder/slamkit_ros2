f54115044bbed90b6861b4c2535aacbd827456ca
Jacob <thejacobcohen@gmail.com>
2026-02-25 23:07:47 -0500

    Cleaned up add udev script so you can run again without add extra lines to the file

--- scripts/add_udev.sh
+++ scripts/add_udev.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/bash
-echo -e 'SUBSYSTEM=="usb", ATTRS{idProduct}=="f100", ATTRS{idVendor}=="fccf", GROUP="users", MODE="0666"\n' >> ./99-fccf-f100.rules
-# change idProduct and idVendor according to actual usb port
-sudo mv ./99-fccf-f100.rules /etc/udev/rules.d/
-udevadm trigger
-sudo udevadm control --reload && sudo udevadm trigger
+sudo echo 'SUBSYSTEM=="usb", ATTRS{idProduct}=="f100", ATTRS{idVendor}=="fccf", GROUP="users", MODE="0666"' \
+  | sudo tee /etc/udev/rules.d/99-fccf-f100.rules
+sudo udevadm control --reload
+sudo udevadm trigger
--- scripts/add_udev_plugdev.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-echo 'SUBSYSTEM=="usb", ATTRS{idProduct}=="f100", ATTRS{idVendor}=="fccf", GROUP="plugdev", MODE="0666"\n' >> ./99-fccf-f100.rules
-# change idProduct and idVendor according to actual usb port
-sudo mv ./99-fccf-f100.rules /etc/udev/rules.d/
-sudo udevadm trigger
-sudo udevadm control --reload && sudo udevadm trigger
